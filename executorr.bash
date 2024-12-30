efaf3c4eefbd465d7534a5c350ed3ed623dca963
droid254s-pec <bikaze50@gmail.com>
2024-12-30 20:14:24 +0300

    Create executorr.bash

---
+++ executorr.bash
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Ensure script is executed with root privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root using sudo."
+  exit
+fi
+
+# Update package lists
+sudo apt update
+
+# Install required packages
+sudo apt install -y build-essential git screen
+
+# Start a new screen session
+screen -dmS t3rn
+
+# Navigate to screen session and execute commands
+screen -S t3rn -X stuff "
+# Download executor release
+wget https://github.com/t3rn/executor-release/releases/download/v0.31.0/executor-linux-v0.31.0.tar.gz
+
+# Extract the downloaded archive
+tar -xvzf executor-linux-v0.31.0.tar.gz
+
+# Navigate to the executor directory
+cd executor/executor/bin
+
+# Export environment variables
+export NODE_ENV=testnet
+export LOG_LEVEL=debug
+export LOG_PRETTY=false
+export EXECUTOR_PROCESS_ORDERS=true
+export EXECUTOR_PROCESS_CLAIMS=true
+export EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=false
+export RPC_ENDPOINTS_ARBT='ENTER API KEY'
+export RPC_ENDPOINTS_OPSP='ENTER API KEY'
+export RPC_ENDPOINTS_L1RN='https://brn.rpc.caldera.xyz'
+export PRIVATE_KEY_LOCAL='ENTER YOUR_PRIVATE_KEY'
+export ENABLED_NETWORKS='base-sepolia,arbitrum-sepolia,optimism-sepolia,blast-sepolia,l1rn'
+export EXECUTOR_MAX_L3_GAS_PRICE=300
+
+# Run the executor
+./executor
+"
